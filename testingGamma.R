00ceebf207014d7ab6de4e1750b4d112fb169897
Paula Pappalardo <paulapappalardo@gmail.com>
2015-04-23 14:09:43 -0400

    gamma ready, working on theta and segsites
    You can see the gamma plots in pdf files for each of the 4 populations
    of 1000 individuals. How I did that is in the file "running simulations"

--- testingGamma.R
+++ testingGamma.R
@@ -3,12 +3,13 @@
 # know the real number of samples
 # and have haplotype diversity from the original field population
 
-numhaps<-c(7,8,6,8,8,13,12,6,12,9) # I will have this vector for real
-numsamp<-rep(18,10) #niter in MS
-hapdiv<-c(0.71,0.76,0.58,0.74,0.80,0.84,0.84,0.61,0.84,0.72) #minvalue 0.001 if no information; this is PRIOR information
+# Population 1 (theta=2, no growth)
+numhaps<-c(1,2,3,4,6,6,9) # I will have this vector for real
+numsamp<-c(2,4,8,16,32,64,128) #niter in MS
+hapdiv<-c(0.5504204, 0.3377538, 0.8915536, 0.9258679) #minvalue 0.001 if no information; this is PRIOR information
 
 # set plot space depending number of datasets
-par(mfrow=c(2,5))
+par(mfrow=c(2,4),oma=c(0,0,2,0))
 
 # loop through all the haplotypes and generate plots
 for(i in 1:length(numhaps)){
@@ -21,7 +22,7 @@ while (cdf<0.99) {
   #  cdf<-pgamma(x,1,Hapdiv) 
   #  if use 1 as shape parameter keeping shape parameter constant doesn't account for increased variance (?) as numhaps go up, 
   #  e.g. error may be higher as you observe more...once it is working run it by somebody mathier.
-  cdf<-pgamma(x,numhaps[i],hapdiv[i]) #might be that numhaps is actually the shape parameter!!!! or: something else...non-gamma.
+  cdf<-pgamma(x,numhaps[i],hapdiv[4]) #might be that numhaps is actually the shape parameter!!!! or: something else...non-gamma.
   indprob<-cdf-cdfprev
     
   happrob<-numhaps[i]+(x-1)
@@ -35,6 +36,7 @@ while (cdf<0.99) {
   }
 probs<-t(matrix(array,nrow=3))
 probs
-title<-paste(c("n=",numSamp[i]))
+title<-paste(c("n=",numsamp[i],sep=""))
 plot(probs[,1],probs[,3],col='red',main=title)
+mtext("Pop4:Theta 10-no growth",outer=T)
 }
